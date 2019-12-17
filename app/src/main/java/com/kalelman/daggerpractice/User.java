abb8184290199e957c3ddfe10a03817a0efb391b
kalelman <erick_rojas_perez@hotmail.com>
2019-12-16 19:18:35 -0600

    Application Component

---
+++ app/src/main/java/com/kalelman/daggerpractice/User.java
@@ -0,0 +1,12 @@
+package com.kalelman.daggerpractice;
+
+public class User {
+
+    private int id;
+    private String username;
+
+    public User(int id, String username) {
+        this.id = id;
+        this.username = username;
+    }
+}
